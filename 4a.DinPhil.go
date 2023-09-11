64348ed318560d75317a0452e232899ce8b65eaa
Anand Patil <anandpatil7026@gmail.com>
2023-09-11 18:51:52 +0530

    Update 4a.DinPhil.go

--- 4a.DinPhil.go
+++ 4a.DinPhil.go
@@ -102,3 +102,39 @@ func main() {
 // Philosopher 0 is thinking
 // Philosopher 2 is eating
 // Philosopher 0 is eating
+package main
+import(
+    "fmt"
+    "sync"
+    "time"
+)
+type Chopstick struct{
+    sync.Mutex
+}
+func main(){
+    var numPhilosophers,dd int
+    fmt.Print("Enter no. of philosophers: ")
+    fmt.Scan(&numPhilosophers)
+    fmt.Printf("Enter the maximum dining cycles",dd)
+    fmt.Scan(&dd)
+    var wg sync.WaitGroup
+    chopsticks:=make([]Chopstick,numPhilosophers)
+    for i:=0;i<numPhilosophers;i++{
+        wg.Add(1)
+        go func(id int){
+            defer wg.Done()
+            fmt.Printf("Philosopher %d is thinking\n",id)
+            time.Sleep(time.Second)
+            left:=id
+            right:=(id+dd)%numPhilosophers
+            chopsticks[left].Lock()
+            chopsticks[right].Lock()
+            fmt.Printf("Philosopher %d is eating\n",id)
+            time.Sleep(time.Second)
+            fmt.Printf("Philosopher %d finished eating\n",id)
+            chopsticks[left].Unlock()
+            chopsticks[right].Unlock()
+        }(i)
+    }
+    wg.Wait()
+}
